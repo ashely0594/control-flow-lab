01b609c849c57857e32f02d7f21bd5651da19d4f
Dominguez <ashely.dominguez@accenture.com>
2024-09-23 12:32:09 -0400

    part 1

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// plants double in number every week 
+//predict plant growth after a specific number of weeks 
+// control flow. pruned to stop them from exceeding the capacity of the garden 
+// monitored if they are growing at an acceptable rate plant count is between 50% and 80% 
+// planted if there is room to plant more plants 
+// plant count is less than 50% of the maximum capacity of the garden 
+// week 1 week 2 week 3 
+
+const PI= 3.1415; //pie radius
+let radius = 5;   //radius of plants
+let A = PI * ((r) **2)  //Area formu
+const MiniSpaceArea = 0.8 
+let WeekPlants1 = 20; 
+
+//Part 1 Growing Pains 
+
+
