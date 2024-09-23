af25c49b610b48c3c12e1abe740471c90c98ba6e
Dominguez <ashely.dominguez@accenture.com>
2024-09-23 14:24:02 -0400

    added part 2

--- main.js
+++ main.js
@@ -8,11 +8,44 @@
 // week 1 week 2 week 3 
 
 const PI= 3.1415; //pie radius
-let radius = 5;   //radius of plants
+let r = 5;   //radius of plants
 let A = PI * ((r) **2)  //Area formu
 const MiniSpaceArea = 0.8 
 let WeekPlants1 = 20; 
 
+
 //Part 1 Growing Pains 
+
+console.log(A) //total area 78.5375
+let maximumCapacity = Math.round(A/ MiniSpaceArea)
+console.log(maximumCapacity) // maximum capacity is 98
+
+let weekPlants2 = WeekPlants1 * 2  //40
+let weekPlants3 = weekPlants2 * 2 //80
+console.log(weekPlants3) 
+
+let Pruned = .80 * maximumCapacity //78.4
+console.log(Pruned)
+
+let Monitored = .50 * maximumCapacity //49
+console.log(Monitored)
+
+//Conditionals 
+
+
+if (weekPlants3 > Pruned) {
+    console.log('Prune the garden');
+} 
+else if ( Monitored < weekPlants2  && weekPlants2 < Pruned) {
+    console.log('Garden is monitored');
+} 
+else if (weekPlants2 < Monitored) {
+    console.log ('Plant more');
+}
+
+
+
+
+
 
 
